d37d5fe3b650b4e36772872b475924a0e9224a10
Donovan Williams <dw58@email.sc.edu>
2023-10-06 16:09:25 -0400

    arrays

--- assignments/assignment9/script.js
+++ assignments/assignment9/script.js
@@ -1,58 +1,29 @@
-// let str = "This is an amazing sentence.";
-// let words = str.split(" ");
+let count = 0;
+
 const quote = () => {
-    var str = ["Dang it gavin","Ayooooooooooooo", "whats up", "i dont know"];
-    // var words = str.split("");
-    // let str2 = ["Dang igavin"]
-    // let str3 = ["Dang it gavin"]
-    
-    // const resultDiv = document.getElementById("quotes");
-    // const section = document.createElement("section");
-    // resultDiv.append(section);
-    // const h3 = document.createElement("h3");
-    // section.append(h3);
-    // const p = document.createElement("p");
-    // section.append(p);
-    // const p2 = document.createElement("p");
-    // section.append(p2);
-    
+    const resultDiv = document.getElementById("quotes");
+    const section = document.createElement("section");
+    resultDiv.append(section);
+    const h3 = document.createElement("h3");
+    section.append(h3);
+    const p = document.createElement("p");
+    section.append(p);
+    const p2 = document.createElement("p");
+    section.append(p2);
+
     var result = document.getElementById("quotes");
-    var result2 = document.getElementById("quotes");
-    
-    
-    
-    const update = setInterval(() => {
 
-    //     for (let i in str) {
-    //     // str.forEach(([]) => {
-    //         result.innerHTML += str[i];
-    //     // }); 
-    // }
-    for (let i in str) {
-        if(i = 0) {
-            result.innerHTML = str[0];
-        }
-        if(i=5) {
-            result.replaceWith(result2) = str[1];
-        }
-        
-        // let i = 0;
-        // i++;
-        // if (i >= str.length) {
-        //    i==0;
-        // }
-        // if (i = 1) {
-        //     p2.textContent = str[0]; 
-        // } 
-        // if( i = 2) {
-        //     p2.textContent = str[1]; 
-        }
-                           
-    
-    }, 5);
-    
+    var str = ["Darkness cannot drive out darkness, only light can do that. Hate cannot drive out hate, only love can do that. -Martin Luther King Jr.","The successful warrior is the average man, with laser-like focus. - Bruce Lee", "The only thing we have to fear is fear itself. -Franklin D. Roosevelt", "Try not to become a man of success. Rather become a man of value. -Albert Einstein", "With great power comes great responsibility. -Stan Lee"];
+    result.innerHTML = str[count];
+    count++;
 
-}
+    
+    let nextQuote = str[count];
+
+    if (!nextQuote) {
+        count = 0;
+    }
+};
 
 const draw = () => {
     console.log("test");
@@ -60,8 +31,8 @@ const draw = () => {
 
 
 window.onload = () => {
-    quote();
     document.getElementById("display-rainbow").onclick = draw; 
+    setInterval(quote, 1000);
 }
 
 
