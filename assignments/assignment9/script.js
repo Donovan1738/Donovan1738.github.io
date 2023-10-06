5fcb2c8bf078bf565fd2f842d37e79f649470e93
Donovan Williams <dw58@email.sc.edu>
2023-10-06 17:30:17 -0400

    arrays

--- assignments/assignment9/script.js
+++ assignments/assignment9/script.js
@@ -13,7 +13,7 @@ const quote = () => {
 
     var result = document.getElementById("quotes");
 
-    var str = ["Darkness cannot drive out darkness, only light can do that. Hate cannot drive out hate, only love can do that. -Martin Luther King Jr.","The successful warrior is the average man, with laser-like focus. - Bruce Lee", "The only thing we have to fear is fear itself. -Franklin D. Roosevelt", "Try not to become a man of success. Rather become a man of value. -Albert Einstein", "With great power comes great responsibility. -Stan Lee"];
+    var str = ["Only a life lived for others is a life lived worthwhile. -Albert Einstein","The successful warrior is the average man, with laser-like focus. - Bruce Lee", "Breathe. It's just a bad day, not a bad life. -Johnny Depp", "With great power comes great responsibility. -Stan Lee"];
     result.innerHTML = str[count];
     count++;
 
@@ -26,7 +26,51 @@ const quote = () => {
 };
 
 const draw = () => {
-    console.log("test");
+    
+    document.getElementById("gold").classList.add("hidden");
+    setInterval(myRainbow, 0);
+    document.getElementById("red").classList.add("hidden");
+    setInterval(myOrange, 1000);
+    document.getElementById("orange").classList.add("hidden");
+    setInterval(myYellow, 2000);
+    document.getElementById("yellow").classList.add("hidden");
+    setInterval(myGreen, 3000);
+    document.getElementById("green").classList.add("hidden");
+    setInterval(myBlue, 4000);
+    document.getElementById("blue").classList.add("hidden");
+    setInterval(myIndigo, 5000);
+    document.getElementById("indigo").classList.add("hidden");
+    setInterval(myGold, 6001);
+
+    clearInterval();
+
+    function myRainbow() {
+        document.getElementById("red").classList.remove("hidden");
+        colorRed(background = 80);
+    }
+    function myOrange() {
+        document.getElementById("orange").classList.remove("hidden");
+        colorRed(background = 80);
+    }
+    function myYellow() {
+        document.getElementById("yellow").classList.remove("hidden");
+        colorRed(background = 80);
+    }
+    function myGreen() {
+        document.getElementById("green").classList.remove("hidden");
+        colorRed(background = 80);
+    }
+    function myBlue() {
+        document.getElementById("blue").classList.remove("hidden");
+        colorRed(background = 80);
+    }
+    function myIndigo() {
+        document.getElementById("indigo").classList.remove("hidden");
+        colorRed(background = 80);
+    }
+    function myGold() {
+        document.getElementById("gold").classList.remove("hidden");
+    }
 }
 
 
