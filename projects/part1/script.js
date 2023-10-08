a882cf5d72e8f491a5f0fd3f7ddcdd8849e05103
Donovan Williams <dw58@email.sc.edu>
2023-10-07 20:49:28 -0400

    project

--- projects/part1/script.js
+++ projects/part1/script.js
@@ -4,7 +4,6 @@ const showNav = () => {
     const nav2 = document.getElementById("hi3").classList.toggle("hide");
     const nav3 = document.getElementById("hi4").classList.toggle("hide");
     const nav4 = document.getElementById("hi5").classList.toggle("hide");
-   
 }
 
 window.onload = () => {
