fa70f211dbb12d8968b4607d1f06f76df0cbb00d
Donovan Williams <dw58@email.sc.edu>
2023-10-08 17:24:43 -0400

    arrays

--- projects/part1/page5/script.js
+++ projects/part1/page5/script.js
@@ -18,7 +18,7 @@ const displayReview = () => {
     section.append(h2);
     const h3 = document.createElement("h3");
     section.append(h3);
-    const p = document.createElement("p");
+    const p = document.createElement("p"); 
     section.append(p)
 
     h2.textContent = `Reviewer: ${name}`;
