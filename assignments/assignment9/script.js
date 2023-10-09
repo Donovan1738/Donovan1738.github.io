6e09e3fd3105053a5e7f1815dabd478253d0fd6b
Donovan Williams <dw58@email.sc.edu>
2023-10-08 21:59:46 -0400

    arrays

--- assignments/assignment9/script.js
+++ assignments/assignment9/script.js
@@ -1,9 +1,9 @@
 let count = 0;
 
 const quote = () => {
-    const resultDiv = document.getElementById("quotes");
+    const quoteDiv = document.getElementById("quotes");
     const section = document.createElement("section");
-    resultDiv.append(section);
+    quoteDiv.append(section);
     const h3 = document.createElement("h3");
     section.append(h3);
     const p = document.createElement("p");
@@ -11,14 +11,13 @@ const quote = () => {
     const p2 = document.createElement("p");
     section.append(p2);
 
-    var result = document.getElementById("quotes");
+    let result = document.getElementById("quotes");
 
-    var str = ["Only a life lived for others is a life lived worthwhile. -Albert Einstein","The successful warrior is the average man, with laser-like focus. - Bruce Lee", "Breathe. It's just a bad day, not a bad life. -Johnny Depp", "With great power comes great responsibility. -Stan Lee"];
-    result.innerHTML = str[count];
+    let quote = ["Only a life lived for others is a life lived worthwhile. -Albert Einstein","The successful warrior is the average man, with laser-like focus. - Bruce Lee", "Breathe. It's just a bad day, not a bad life. -Johnny Depp", "With great power comes great responsibility. -Stan Lee"];
+    result.innerHTML = quote[count];
     count++;
 
-    
-    let nextQuote = str[count];
+    let nextQuote = quote[count];
 
     if (!nextQuote) {
         count = 0;
@@ -27,49 +26,49 @@ const quote = () => {
 
 const draw = () => {
     
-    document.getElementById("gold").classList.add("hidden");
-    setInterval(myRainbow, 0);
-    document.getElementById("red").classList.add("hidden");
-    setInterval(myOrange, 1000);
-    document.getElementById("orange").classList.add("hidden");
-    setInterval(myYellow, 2000);
-    document.getElementById("yellow").classList.add("hidden");
-    setInterval(myGreen, 3000);
-    document.getElementById("green").classList.add("hidden");
-    setInterval(myBlue, 4000);
-    document.getElementById("blue").classList.add("hidden");
-    setInterval(myIndigo, 5000);
-    document.getElementById("indigo").classList.add("hidden");
-    setInterval(myGold, 6001);
+    document.getElementById("gold").classList.add("hide");
+    setInterval(red, 0);
+    document.getElementById("red").classList.add("hide");
+    setInterval(orange, 1000);
+    document.getElementById("orange").classList.add("hide");
+    setInterval(yellow, 2000);
+    document.getElementById("yellow").classList.add("hide");
+    setInterval(green, 3000);
+    document.getElementById("green").classList.add("hide");
+    setInterval(blue, 4000);
+    document.getElementById("blue").classList.add("hide");
+    setInterval(indigo, 5000);
+    document.getElementById("indigo").classList.add("hide");
+    setInterval(gold, 6000);
 
     clearInterval();
 
-    function myRainbow() {
-        document.getElementById("red").classList.remove("hidden");
+    function red() {
+        document.getElementById("red").classList.remove("hide");
         colorRed(background = 80);
     }
-    function myOrange() {
-        document.getElementById("orange").classList.remove("hidden");
+    function orange() {
+        document.getElementById("orange").classList.remove("hide");
         colorRed(background = 80);
     }
-    function myYellow() {
-        document.getElementById("yellow").classList.remove("hidden");
+    function yellow() {
+        document.getElementById("yellow").classList.remove("hide");
         colorRed(background = 80);
     }
-    function myGreen() {
-        document.getElementById("green").classList.remove("hidden");
+    function green() {
+        document.getElementById("green").classList.remove("hide");
         colorRed(background = 80);
     }
-    function myBlue() {
-        document.getElementById("blue").classList.remove("hidden");
+    function blue() {
+        document.getElementById("blue").classList.remove("hide");
         colorRed(background = 80);
     }
-    function myIndigo() {
-        document.getElementById("indigo").classList.remove("hidden");
+    function indigo() {
+        document.getElementById("indigo").classList.remove("hide");
         colorRed(background = 80);
     }
-    function myGold() {
-        document.getElementById("gold").classList.remove("hidden");
+    function gold() {
+        document.getElementById("gold").classList.remove("hide");
     }
 }
 
