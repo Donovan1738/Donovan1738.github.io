83e5b1de7b190de850977eee4f11a3935f891100
Donovan Williams <dw58@email.sc.edu>
2023-10-12 18:43:04 -0400

    classes

--- assignments/assignment10/script.js
+++ assignments/assignment10/script.js
@@ -11,29 +11,28 @@ class Toy {
         const section = document.createElement("section");
         section.classList.add("toy");
 
-        const h3 = document.createElement("h3");
-        h3.innerHTML = this.title;
-        section.append(h3);
-        h3.classList.add("hidden");
+        // const h3 = document.createElement("h3");
+        // h3.innerHTML = this.title;
+        // section.append(h3);
+        // h3.classList.add("hidden");
        
 
         const ul = document.createElement("p");
         // ul.classList.add("hi");
         section.append(ul);
         ul.classList.add("hidden");
-        ul.append(this.listItem(this.rating));
-        ul.append(this.listItem(this.price));
-        ul.append(this.listItem("age: " + this.age));
+        ul.append(this.listItem(this.title));
+        ul.append(this.listItem("Rating: " + this.rating));
+        ul.append(this.listItem("Price: " + this.price));
+        ul.append(this.listItem("Age Range: " + this.age));
 
         section.append(this.picture(this.pic));
 
         section.onmouseover = () => {
             ul.classList.remove("hidden");
-            h3.classList.remove("hidden");
         }
         section.onmouseout = () => {
             ul.classList.add("hidden");
-            h3.classList.add("hidden");
         }
         return section;
     }
@@ -55,12 +54,12 @@ class Toy {
 const showToys = () => {
     const toyList = document.getElementById("toy-list");
     const toys = [];
-    toys.push(new Toy("Lego", "$16", 12, "4 stars", "car.jpg"));
-    toys.push(new Toy("Car", "$16", 12, "3 stars", "car.jpg"));
-    toys.push(new Toy("Basketball", "$16", 12, "2 stars", "car.jpg"));
-    toys.push(new Toy("Lego", "$16", 12, "4 stars", "car.jpg"));
-    toys.push(new Toy("Car", "$16", 12, "3 stars", "car.jpg"));
-    toys.push(new Toy("Basketball", "$16", 12, "2 stars", "car.jpg"));
+    toys.push(new Toy("Lego", "$11", "8-15", "5 stars", "lego.jpg"));
+    toys.push(new Toy("Car", "$4", "4-12", "4.5 stars", "car.jpg"));
+    toys.push(new Toy("Nerf Gun", "$16", "12-16", "4.8 stars", "nerf.jpg"));
+    toys.push(new Toy("Helicopter", "$30", "12-16", "4 stars", "copter.webp"));
+    toys.push(new Toy("Skateboard", "$100", "10-18", "3 stars", "board.jpg"));
+    toys.push(new Toy("Basketball", "$20", "6+", "2 stars", "ball.avif"));
 
     toys.forEach(toy => {
         toyList.append(toy.toyItem);
