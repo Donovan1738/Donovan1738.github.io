9bc977628f04c87aed4938cb98918b08ce823db0
Donovan Williams <dw58@email.sc.edu>
2023-10-28 15:26:03 -0400

    json

--- projects/part4/script.js
+++ projects/part4/script.js
@@ -6,6 +6,64 @@ const showNav = () => {
     const nav4 = document.getElementById("hi5").classList.toggle("hide");
 }
 
+const getProject = async () => {
+    const url = "https://donovan1738.github.io/projects/part4/project.json"
+
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    }   catch (error) {
+        console.log(error);
+    }
+};
+
+const showProject = async () => {
+    let movies = await getProject();
+    let movieSection = document.getElementById("yo");
+
+    movies.forEach((movie) => {
+        movieSection.append(getMovieItem(movie));
+    });
+};
+
+const getMovieItem = (movie) => {
+    // let section = document.createElement("section");
+    let div = document.createElement("div");
+
+    let h8 = document.createElement("img");
+    // let url = "https://portiaportia.github.io/json/";
+    h8.src = movie.img;
+    section.append(h8);
+
+    // section.append(div);
+    // let h2 = document.createElement("h2");
+    // h2.innerText = movie.title;
+    // div.append(h2);
+
+    // let h3 = document.createElement("p");
+    // h3.innerText = "Director: " + movie.director;
+    // div.append(h3);
+
+    // let h4 = document.createElement("p");
+    // h4.innerText = "Actors: " + movie.actors;
+    // div.append(h4);
+    
+    // let h5 = document.createElement("p");
+    // h5.innerText = "Year Released: " + movie.year;
+    // div.append(h5);
+
+    // let h6 = document.createElement("p");
+    // h6.innerText = "Genres: " +  movie.genres;
+    // div.append(h6);
+
+    // let h7 = document.createElement("p");
+    // h7.innerText = movie.description;
+    // div.append(h7);
+    
+    return section;
+}
+
 window.onload = () => {
     document.getElementById("nav-toggle").onclick = showNav;
+    showProject();
 }
