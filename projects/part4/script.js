489ec06179d1b7ac7fbe9795f83a4ce19cfb4492
Donovan Williams <dw58@email.sc.edu>
2023-10-28 15:39:07 -0400

    json

--- projects/part4/script.js
+++ projects/part4/script.js
@@ -18,49 +18,33 @@ const getProject = async () => {
 };
 
 const showProject = async () => {
-    let movies = await getProject();
-    let movieSection = document.getElementById("yo");
+    let pros = await getProject();
+    let proSection = document.getElementById("yo");
 
-    movies.forEach((movie) => {
-        movieSection.append(getMovieItem(movie));
+    pros.forEach((pro) => {
+        proSection.append(getProItem(pro));
     });
 };
 
-const getMovieItem = (movie) => {
-    // let section = document.createElement("section");
-    let div = document.createElement("div");
+const getProItem = (pro) => {
+    let div = document.getElementById("yo");
 
     let h8 = document.createElement("img");
-    // let url = "https://portiaportia.github.io/json/";
-    h8.src = movie.img;
-    section.append(h8);
+    h8.src = pro.img;
+    h8.classList.add("pic");
+    div.append(h8);
 
-    // section.append(div);
-    // let h2 = document.createElement("h2");
-    // h2.innerText = movie.title;
-    // div.append(h2);
+    let h2 = document.createElement("h2");
+    h2.innerText = pro.title;
+    h2.classList.add("over");
+    div.append(h2);
 
-    // let h3 = document.createElement("p");
-    // h3.innerText = "Director: " + movie.director;
-    // div.append(h3);
-
-    // let h4 = document.createElement("p");
-    // h4.innerText = "Actors: " + movie.actors;
-    // div.append(h4);
+    let h3 = document.createElement("p");
+    h3.innerText = pro.description;
+    h3.classList.add("p1");
+    div.append(h3);
     
-    // let h5 = document.createElement("p");
-    // h5.innerText = "Year Released: " + movie.year;
-    // div.append(h5);
-
-    // let h6 = document.createElement("p");
-    // h6.innerText = "Genres: " +  movie.genres;
-    // div.append(h6);
-
-    // let h7 = document.createElement("p");
-    // h7.innerText = movie.description;
-    // div.append(h7);
-    
-    return section;
+    return div;
 }
 
 window.onload = () => {
